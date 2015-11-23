5ad108e6cad6246003d015efbf6a87cb9eb9c39b
20063251 <20063251@it102imac15.wit.ie>
2015-11-23 14:43:26 +0000

    Loop kick button implemented

--- AudioTouchPad/ViewController.swift
+++ AudioTouchPad/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var kickButton : AVAudioPlayer?
+    
+    func setupAudioPlayerWithFile(file:NSString, type:NSString) -> AVAudioPlayer?  {
+        //1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        //2
+        var audioPlayer:AVAudioPlayer?
+        
+        // 3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not available")
+        }
+        
+        return audioPlayer
     }
-
+    
+    override func viewDidLoad() {
+        if let kickButton = self.setupAudioPlayerWithFile("kick", type:"wav") {
+            self.kickButton = kickButton
+        }
+        super.viewDidLoad()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func playKick(sender: AnyObject) {
+        kickButton?.prepareToPlay()
+        kickButton?.play()
+        kickButton?.numberOfLoops = -1
+    }
 }
 
