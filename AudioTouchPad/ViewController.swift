c8f5048e82d18ceedab2d814070fbc5cd5e048f8
20063251 <20063251@it102imac15.wit.ie>
2015-11-30 14:57:27 +0000

    Cleaned up code by making generic methods for each sound rather than hardcoding it in, and also put in checks for looping and stopping in the playSound method itself, so I no longer have three separate methods for playing, looping and stopping music

--- AudioTouchPad/ViewController.swift
+++ AudioTouchPad/ViewController.swift
@@ -46,6 +46,7 @@ class ViewController: UIViewController {
             self.synthSound = synthSound
         }
         super.viewDidLoad()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -53,84 +54,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var isLoop = false
+    func playSound(audio: AVAudioPlayer, button: UIButton, inout isLoop: Bool){
+        if (loop == false && isLoop == false){
+            audio.prepareToPlay()
+            audio.play()
+            audio.numberOfLoops = 0
+            isLoop = true
+            button.backgroundColor = UIColor.darkGrayColor()
+        }
+        else if (loop == true && isLoop == false){
+            audio.prepareToPlay()
+            audio.play()
+            audio.numberOfLoops = -1
+            isLoop = true
+            button.backgroundColor = UIColor.cyanColor()
+        }
+        else {
+            audio.stop()
+            audio.currentTime = 0
+            button.backgroundColor = UIColor.darkGrayColor()
+            isLoop = false
+        }
+    }
+    
+    
     //Kick 1 Button
     @IBOutlet weak var kickButton1: UIButton!
     var kickButton1Loop = false
     @IBAction func playKick1(sender: AnyObject) {
-        if (loop == true && kickButton1Loop == false){
-            kickSound1?.prepareToPlay()
-            kickSound1?.play()
-            kickSound1?.numberOfLoops = -1
-            kickButton1.backgroundColor = UIColor.cyanColor()
-            kickButton1Loop = true
-        }
-        else if (loop == false && kickButton1Loop == false){
-            kickSound1?.play()
-            kickSound1?.numberOfLoops = 0
-            kickButton1.backgroundColor = UIColor.darkGrayColor()
-            kickButton1Loop = true
-        }
-        else{
-            kickSound1?.stop()
-            kickSound1?.currentTime = 0
-            kickButton1.backgroundColor = UIColor.darkGrayColor()
-            kickButton1Loop = false
-        }
+        playSound(kickSound1!, button: kickButton1, isLoop: &kickButton1Loop)
     }
     
     //Kick 2 Button
     @IBOutlet weak var kickButton2: UIButton!
     var kickButton2Loop = false
     @IBAction func playKick2(sender: AnyObject) {
-        if (loop == true && kickButton2Loop == false){
-            kickSound2?.prepareToPlay()
-            kickSound2?.play()
-            kickSound2?.numberOfLoops = -1
-            kickButton2.backgroundColor = UIColor.cyanColor()
-            kickButton2Loop = true
-        }
-        else if (loop == false && kickButton2Loop == false){
-            kickSound2?.play()
-            kickSound2?.numberOfLoops = 0
-            kickButton2.backgroundColor = UIColor.darkGrayColor()
-            kickButton2Loop = true
-        }
-        else {
-            kickSound2?.stop()
-            kickSound2?.currentTime = 0
-            kickButton2.backgroundColor = UIColor.darkGrayColor()
-            kickButton2Loop = false
-        }
+        playSound(kickSound2!, button: kickButton2, isLoop: &kickButton2Loop)
     }
     
     //Synth Music Button
     @IBOutlet weak var synthButton: UIButton!
     var synthButtonLoop = false
     @IBAction func playSynth(sender: AnyObject) {
-        if (loop == true && synthButtonLoop == false){
-            synthSound?.prepareToPlay()
-            synthSound?.play()
-            synthSound?.numberOfLoops = -1
-            synthButton.backgroundColor = UIColor.cyanColor()
-            synthButtonLoop = true
-        }
-        else if (loop == false && synthButtonLoop == false){
-            synthSound?.play()
-            synthSound?.numberOfLoops = 0
-            synthButton.backgroundColor = UIColor.darkGrayColor()
-            synthButtonLoop = true
-        }
-        else{
-            synthSound?.stop()
-            synthSound?.currentTime = 0
-            synthButton.backgroundColor = UIColor.darkGrayColor()
-            synthButtonLoop = false
-        }
+        playSound(synthSound!, button: synthButton!, isLoop: &synthButtonLoop)
     }
     
     //Loop button
     @IBOutlet weak var loopButton: UIButton!
-    @IBAction func loopSound(sender: AnyObject) {
+    @IBAction func loopToggle(sender: AnyObject) {
         if (loop == false) {
             loop = true
             loopButton.backgroundColor = UIColor.greenColor()
@@ -138,6 +110,25 @@ class ViewController: UIViewController {
         else{
             loop = false
             loopButton.backgroundColor = UIColor.redColor()
+        }
+    }
+    
+    @IBOutlet weak var play1Button: UIButton!
+    var play1ButtonLoop = false
+    @IBAction func play1Sound(sender: AnyObject) {
+        playSound(synthSound!, button: synthButton, isLoop: &synthButtonLoop)
+        playSound(kickSound1!, button: kickButton1, isLoop: &kickButton1Loop)
+        if (loop == true && play1ButtonLoop == false){
+            play1ButtonLoop = true
+            play1Button.backgroundColor = UIColor.cyanColor()
+        }
+        else if (loop == false && kickButton2Loop == false){
+            play1ButtonLoop = true
+            play1Button.backgroundColor = UIColor.darkGrayColor()
+        }
+        else {
+            play1ButtonLoop = false
+            play1Button.backgroundColor = UIColor.darkGrayColor()
         }
     }
 }
